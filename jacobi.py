d79c7af0d3841cc223cc865763968148a2947551
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-06-07 21:13:02 -0300

    Carga b

--- jacobi.py
+++ jacobi.py
@@ -10,3 +10,14 @@ for i in range(1, n + 1):
         except:
             A[i] = {1: 0}
         A[i][j] = float(input("A[" + str(i) + "-" + str(j) + "]= "))
+
+b = {
+	1: 0
+}
+
+for i in range(1, n + 1):
+    try:
+        b[i]
+    except:
+        b[i] = 0
+    b[i] = float(input("b[" + str(i) + "]= "))
