5b4ab013da93a4f6633cb811587718cbe7867b8f
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-06-07 21:11:45 -0300

    Carga A

--- jacobi.py
+++ jacobi.py
@@ -1,21 +1,12 @@
-def cargoMatriz(n):
-	matriz = []
+n = int(input("Tamanio = "))
 
-	print("Ingrese los valores de la matriz A: \n")
-	for i in range(0, n):
-		fila = []
-
-		print("Insertando los elementos de la fila " + str(i + 1) + "")
-
-		for j in range(0, n):
-			fila.append(float(input("Ingrese el elemento " + str(j + 1) +
-                           " (A[" + str(i + 1) + "][" + str(j + 1) + "]): ")))
-
-		matriz.append(fila)
-		print("")
-
-	return matriz
-
-
-n = input("Tamanio = ")
-prueba = cargoMatriz(n)
+A = {
+    1: {1: 0}
+}
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        try:
+            A[i]
+        except:
+            A[i] = {1: 0}
+        A[i][j] = float(input("A[" + str(i) + "-" + str(j) + "]= "))
