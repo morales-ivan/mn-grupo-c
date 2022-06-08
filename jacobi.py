fbb46293890a077e9be31b28e430d040a652391b
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-06-07 21:21:07 -0300

    verificacion dominante

--- jacobi.py
+++ jacobi.py
@@ -24,6 +24,19 @@ def cargarColumna(n):
 		col[i] = float(input("b[" + str(i) + "]= "))
 
 
+def esMatrizDominante(matriz):
+	tamanio = len(matriz)
+	for i in range(1, tamanio+1):
+		valorDiagonal = abs(matriz[i][i])
+		suma = 0
+		for j in range(1, tamanio+1):
+			if i == j:
+				continue
+			suma += abs(matriz[i][j])
+		if(suma <= valorDiagonal):
+			return False
+	return True
+
 
 n = int(input("Tamanio = "))
 A = cargaMatriz(n)
