2afed47296f91a05d7f3c9505473f7123b861a65
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-06-07 21:28:58 -0300

    Fin carga datos y verificaciones

--- jacobi.py
+++ jacobi.py
@@ -1,4 +1,4 @@
-def cargaMatriz(n):
+def cargarMatriz(nombre, n):
 	matriz = {
 		1: {1: 0}
 	}
@@ -8,20 +8,19 @@ def cargaMatriz(n):
 				matriz[i]
 			except:
 				matriz[i] = {1: 0}
-			matriz[i][j] = float(input("A[" + str(i) + "-" + str(j) + "]= "))
+			matriz[i][j] = float(input(nombre + "[" + str(i) + "-" + str(j) + "]= "))
 	return matriz
 
-def cargarColumna(n):
+def cargarColumna(nombre, n):
 	col = {
 		1: 0
 	}
-
 	for i in range(1, n + 1):
 		try:
 			col[i]
 		except:
 			col[i] = 0
-		col[i] = float(input("b[" + str(i) + "]= "))
+		col[i] = float(input(nombre + "[" + str(i) + "]= "))
 
 
 def esMatrizDominante(matriz):
@@ -39,8 +38,17 @@ def esMatrizDominante(matriz):
 
 
 n = int(input("Tamanio = "))
-A = cargaMatriz(n)
-b = cargarColumna(n)
+A = cargarMatriz("A", n)
+b = cargarColumna("b", n)
+tol = float(input("Tolerancia = "))
+if(tol < 0.00001):
+	print("La tolerancia es muy pequeña")
+	exit()
+N = int(input("Numero maximo de Iteraciones = "))
+if(N > 10000):
+	print("El numero de iteraciones es mas que 10000")
+	exit()
+X0 = cargarColumna("X0", n)
 if(not esMatrizDominante(A)):
 	print("A no es matriz diagonal estrictamente dominante")
 	exit()
