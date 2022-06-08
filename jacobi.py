a402ed80e859ba7075b149c4d5027498e24f4652
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-06-07 21:17:07 -0300

    carga matrices

--- jacobi.py
+++ jacobi.py
@@ -1,23 +1,30 @@
+def cargaMatriz(n):
+	matriz = {
+		1: {1: 0}
+	}
+	for i in range(1, n + 1):
+		for j in range(1, n + 1):
+			try:
+				matriz[i]
+			except:
+				matriz[i] = {1: 0}
+			matriz[i][j] = float(input("A[" + str(i) + "-" + str(j) + "]= "))
+	return matriz
+
+def cargarColumna(n):
+	col = {
+		1: 0
+	}
+
+	for i in range(1, n + 1):
+		try:
+			col[i]
+		except:
+			col[i] = 0
+		col[i] = float(input("b[" + str(i) + "]= "))
+
+
+
 n = int(input("Tamanio = "))
-
-A = {
-    1: {1: 0}
-}
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        try:
-            A[i]
-        except:
-            A[i] = {1: 0}
-        A[i][j] = float(input("A[" + str(i) + "-" + str(j) + "]= "))
-
-b = {
-	1: 0
-}
-
-for i in range(1, n + 1):
-    try:
-        b[i]
-    except:
-        b[i] = 0
-    b[i] = float(input("b[" + str(i) + "]= "))
+A = cargaMatriz(n)
+b = cargarColumna(n)
